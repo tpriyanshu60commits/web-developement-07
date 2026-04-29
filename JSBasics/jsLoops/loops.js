40e992a3931fb2d73612c09490553af895b23152
tpriyanshu60commits <ppriyanshu.ttiwari@gmail.com>
2026-04-29 19:30:42 +0530

    js loops

---
+++ JSBasics/jsLoops/loops.js
@@ -0,0 +1,45 @@
+// for(var i=1 ; i<=10 ; i++)
+// {
+//     console.log(i);
+// }
+
+// for(var i=2 ; i<=20 ; i+=2)
+// {
+//     console.log(i);
+// }
+let a = [24,58,33,86,96,12,53,23,32,100]
+// for(var i=0 ; i<10 ; i++)
+// {
+// console.log(a[i]);
+// }
+
+// let i=0;
+// while(i<10)
+// {
+// console.log(a[i]);
+// i++;
+// }
+
+// let i=0;
+// do
+// {
+//     console.log(i[a]);
+//     i++;
+// }while(i<10);
+
+
+
+// 2 table
+// let i=2;
+// while(i<=20)
+// {
+//     console.log(i);
+//     i+=2;
+// }
+
+let i=2;
+do
+{
+    console.log(i);
+    i+=2;
+}while(i<=20);
