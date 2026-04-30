986e1dd8330431fe73a6a3280e4ab785a532b28c
tpriyanshu60commits <ppriyanshu.ttiwari@gmail.com>
2026-04-30 14:45:03 +0530

    javascript

---
+++ jsPractise/firstCode/script.js
@@ -0,0 +1,13 @@
+// function changeColor()
+// {
+//     document.getElementById("firstcode").innerText = "hello my dear priyanshu";
+// }
+
+function Addbutton()
+{
+    console.log("hi");  
+    const a = document.getElementById("id");
+    const btn =document.createElement("button");
+    btn.innerHTML = "priyanshu";
+    a.append(btn);
+}
