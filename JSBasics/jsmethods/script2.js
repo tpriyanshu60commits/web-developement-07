93629fbf560b80a1f37700495c8d8e1e3d38a0b4
tpriyanshu60commits <ppriyanshu.ttiwari@gmail.com>
2026-05-01 19:48:39 +0530

    refactor: change function declaration to const for consistency

--- JSBasics/jsmethods/script2.js
+++ JSBasics/jsmethods/script2.js
@@ -1,7 +1,24 @@
 // function expression
-let abc = function()
+const abc = function()
 {
     console.log("hello world");
     
 }
 console.log(abc);
+abc();
+
+const bcd = function(a,b)
+{
+    return a+b;
+}
+console.log(bcd(4,5));
+
+const arrowfun = () =>
+{
+    console.log("i am a arrow function");
+}
+arrowfun();
+
+const arrowrow = (a,b) => a + b;
+
+console.log(arrowrow(5,6));
