d52723ff93425cee9841b8c1588d1dcdb4ad77ec
tpriyanshu60commits <ppriyanshu.ttiwari@gmail.com>
2026-05-02 15:04:28 +0530

    conditional-statement assignment

--- JSBasics/conditional-Statement/script.js
+++ JSBasics/conditional-Statement/script.js
@@ -60,12 +60,10 @@ if (age1 >= 18) {
 // Print numbers from 1 to 10.
 // Stop when number becomes 5.
 
-for(let i=1 ; i<=10 ; i++)
-{
+for (let i = 1; i <= 10; i++) {
   console.log(i);
 
-  if(i==5)
-  {
+  if (i == 5) {
     break;
   }
 }
@@ -75,37 +73,30 @@ for(let i=1 ; i<=10 ; i++)
 // Example:
 // When number = 7 → stop loop
 
-for(let i=0 ; i<=10 ; i++)
-{
+for (let i = 0; i <= 10; i++) {
   console.log(i);
-  if(i==7)
-  {
+  if (i == 7) {
     break;
   }
-  
 }
 
 // Exercise 11
 
 // Print numbers from 1 to 5.
 // Skip number 3.
-for(let i=0 ; i<=10 ; i++)
-{
-  if(i==3)
-  { continue;
+for (let i = 0; i <= 10; i++) {
+  if (i == 3) {
+    continue;
   }
   console.log(i);
-  
 }
 // Exercise 12
 
 // Print only even numbers between 1 to 10.
 // Skip odd numbers
 
-for(let i=1 ; i<=10 ; i++)
-{
-  if(i%2==0)
-  { 
+for (let i = 1; i <= 10; i++) {
+  if (i % 2 == 0) {
     console.log(i);
   }
 }
@@ -114,17 +105,205 @@ for(let i=1 ; i<=10 ; i++)
 
 // Create function to check even/odd.
 // Return "Even" or "Odd"
-let b=2;
-function checkEvenOdd(b)
-{
-  if(b % 2 == 0)
-  {
+let b = 2;
+function checkEvenOdd(b) {
+  if (b % 2 == 0) {
     return "Even";
-  }
-  else
-  {
+  } else {
     return "Odd";
   }
+}
+
+// Exercise 15
+
+// Create variable day.
+// Print day name using switch.
+// Example:
+// 1 → Monday
+
+let day = 75;
+switch (day) {
+  case 1:
+    console.log("monday");
+    break;
+  case 2:
+    console.log("Tuesday");
+    break;
+  case 3:
+    console.log("wednesday");
+    break;
+  case 4:
+    console.log("Thursday");
+    break;
+  case 5:
+    console.log("Friday");
+    break;
+  case 6:
+    console.log("saturday");
+    break;
+  case 7:
+    console.log("sunday");
+    break;
+  default:
+    console.log("day doesn't exists");
+}
+// Exercise 16
+
+// Create menu system.
+// Example:
+// 1 → "Tea"
+// 2 → "Coffee"
+// 3 → "Juice"
+let menu = 1;
+switch (menu) {
+  case 1:
+    console.log("tea");
+    break;
+  case 2:
+    console.log("coffee");
+    break;
+  case 3:
+    console.log("juice");
+    break;
+  case 4:
+    console.log("icecream");
+    break;
+  default:
+    console.log("doesn't exists");
+    break;
+}
+// Check fruit name.
+// Example:
+// "Mango" → print message
+// If not matched → "Not available"
+
+let fruit = "mango";
+switch (fruit.toLocaleLowerCase()) {
+  case "mango":
+    console.log("mango available");
+    break;
+
+  default:
+    console.log("not available");
+    break;
+}
+
+// Use switch for calculator.
+// Example:
+
+// → add
+// → subtract
+let c = 1,
+  d = 2;
+let requirement = "add";
+switch (requirement) {
+  case "add":
+    console.log(c + d);
+    break;
+  case "multiply":
+    console.log(c * d);
+    break;
+  case "divide":
+    console.log(c / d);
+    break;
+  case "subtract":
+    console.log(c - d);
+    break;
+  default:
+    console.log("not available");
+}
+
+// 11. Break Understanding
+// Exercise 19
+
+// Create switch without break.
+// Observe how all cases execute.
+// let c=1 , d=2;
+// let requirement = "add";
+// switch(requirement)
+// {
+//   case "add":
+//   console.log(c+d);
+
+//    case "multiply":
+//   console.log(c*d);
+
+//    case "divide":
+//   console.log(c/d);
+//   break;
+
+//    case "subtract":
+//   console.log(c-d);
+
+//   default:
+//     console.log("not available");
+// }
+
+// Exercise 20
+
+// Create program:
+
+// Take marks
+// Use if-else for grade
+// Use switch to display message
+let marks1 = 91;
+let grade;
+if (marks1 >= 90 && marks1 <= 100) {
+  grade = "A";
+} else if (marks1 >= 75 && marks1 < 90) {
+  grade = "B";
+} else if (marks1 >= 50 && marks1 < 75) {
+  grade = "C";
+} else {
+  grade = "D";
+}
+
+switch (grade) {
+  case "A":
+    console.log("topper");
+    break;
+  case "B":
+    console.log("division 2");
+    break;
+  case "C":
+    console.log("division 3");
+    break;
+  default:
+    console.log("fail");
+    break;
+}
+
+// Exercise 22
+
+// Create login system:
+
+// if username correct
+// if password correct
+// else show error
+
+let username1 = "priyanshu60";
+let password1 = 1234;
+if(username1=="priyanshu60" &&password1 == 1234)
+{
+  console.log("login successful");
+}
+else
+{
+  console.log("login unsuccessful");
+}
+
+// Create function:
+
+// take number
+// return "Positive", "Negative", or "Zero"
+let number1 =2;
+if(number1<0)
+{
+  console.log("negative");
+}
+else
+{
+  console.log("positive");
 }
 
 
