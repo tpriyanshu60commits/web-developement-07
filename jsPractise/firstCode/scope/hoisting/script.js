33f8e856bcf4e49e97155223e407efd83e363045
tpriyanshu60commits <ppriyanshu.ttiwari@gmail.com>
2026-05-04 14:40:20 +0530

    hoisting

---
+++ jsPractise/firstCode/scope/hoisting/script.js
@@ -0,0 +1,13 @@
+// console.log(a);
+// console.log(b);
+
+
+// var a ;
+// let b ;
+// Hoisting with Function Expressions
+let greet = function hello()
+{
+    console.log("hello");
+}
+greet();
+console.log(greet());
--- jsPractise/firstCode/scope/script.js
+++ jsPractise/firstCode/scope/script.js
@@ -2,35 +2,37 @@ console.log(a);
 console.log(b);
 
 
-var a = 10;
-let b = 20;
-const c = 30;
-a = 50;
-function hello() {
-  console.log(a);
-  console.log(b);
-  console.log(c);
-}
-hello();
+var a ;
+let b ;
+// const c = 30;
+// a = 50;
+// function hello() {
+//   console.log(a);
+//   console.log(b);
+//   console.log(c);
+// }
+// hello();
 
-function again() {
-  var a = 10;
-  let b = 20;
-  const c = 30;
-  console.log(a);
-  console.log(b);
-  console.log(c);
-  {
-    var d = 40;
-    console.log(d);
-  }
-  console.log(d);
-}
-console.log("again");
-again();
-// Local Scope
-function classRoom() {
-  var teacherName = "Mr. Sharma";
-  console.log(teacherName); // Accessible here
-}
-classRoom();
+// function again() {
+//   var a = 10;
+//   let b = 20;
+//   const c = 30;
+//   console.log(a);
+//   console.log(b);
+//   console.log(c);
+//   {
+//     var d = 40;
+//     console.log(d);
+//   }
+//   console.log(d);
+// }
+// console.log("again");
+// again();
+// // Local Scope
+// function classRoom() {
+//   var teacherName = "Mr. Sharma";
+//   console.log(teacherName); // Accessible here
+// }
+// classRoom();
+
+
