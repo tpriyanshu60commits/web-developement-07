949d33ce939a550cafd119372fc32671b6035dbb
tpriyanshu60commits <ppriyanshu.ttiwari@gmail.com>
2026-05-05 18:03:23 +0530

    assignment-all

--- jsAssignments/DOM-assignment/dom-assignment.js
+++ jsAssignments/DOM-assignment/dom-assignment.js
@@ -1,3 +1,5 @@
+// Assignment 1
+
 function changes()
 {
     let name = document.getElementById("input1").value;
@@ -8,5 +10,23 @@ function changes()
     document.getElementById("mydata").innerText = "Name : " + name;
     document.getElementById("mydata1").innerText ="City : " + city;
     document.getElementById("resultBox").classList.remove("d-none");
-    
+    document.getElementById("mydata").style.color = "blue";
+    document.getElementById("mydata1").style.color = "blue";
+
+}
+
+// Assignment 2
+function changes2(event)
+{
+    event.preventDefault();
+    let email = document.getElementById("email").value;
+    let password = document.getElementById("password").value;
+    console.log(email);
+    console.log(password);
+    document.getElementById("emailData").innerText = "Email : " + email
+    document.getElementById("passwordData").innerText = "password : " + password
+    document.getElementById("resultdiv").style.backgroundColor = "pink";
+    document.getElementById("resultdiv").classList.remove("d-none")
+    // button.remove();
+
 }
