18cdfbecddc1eda694e33028337616332feb7d0b
tpriyanshu60commits <ppriyanshu.ttiwari@gmail.com>
2026-05-17 16:50:43 +0530

    institute form creation done

--- JSBasics/jsProject/ricrRegistrationForm/script.js
+++ JSBasics/jsProject/ricrRegistrationForm/script.js
@@ -4,20 +4,46 @@ document
   .addEventListener("submit", (event) => {
     event.preventDefault();
 
-    const fn = document.getElementById("fullName").value;
-    const gndr = document.querySelector("input[name='gender']:checked")?.value;
+    const fullName = document.getElementById("fullName").value.trim();
+    const email = document.getElementById("email").value.trim();
+    const phone = document.getElementById("phone").value.trim();
+    const dob = document.getElementById("dob").value;
+    const gender = document.querySelector(
+      "input[name='gender']:checked",
+    )?.value;
+    const qualification = document.getElementById("qualification").value;
+    const marks = document.getElementById("marks").value;
+    const course = document.getElementById("course").value;
+    const address = document.getElementById("address").value;
+    const city = document.getElementById("city").value;
+    const pin = document.getElementById("pin").value;
 
     const timings = [];
-
     document
       .querySelectorAll("input[name='timings']:checked")
       .forEach((item) => {
         timings.push(item.value);
       });
 
-    console.log(gndr);
+    const registrationData = {
+      FullName: fullName,
+      Email: email,
+      Phone: phone,
+      DateofBirth: dob,
+      Gender: gender,
+      Qualification: qualification,
+      Marks: marks,
+      Course: course,
+      Timings: timings,
+      Address: address,
+      City: city,
+      PinCode: pin,
+    };
 
-    console.log(timings);
+    validateData(registrationData)
+      ? (console.log("Registration Data:", registrationData),
+        alert("Registration successful! Check console for details."))
+      : alert("Registration unsuccessfull! Enter valid Data.");
   });
 
 // ResetButton
@@ -28,3 +54,36 @@ document
 
     window.location.reload();
   });
+
+function validateData(data) {
+  document.querySelectorAll(".manualError").forEach((item) => {
+    item.innerText = "";
+  });
+
+  let isValid = true;
+
+  console.log(data);
+
+  if (!data.FullName) {
+    document.getElementById("fullNameError").innerText = "Full Name Required";
+    isValid = false;
+  } else if (!/^[A-Za-z\s]+$/.test(data.FullName)) {
+    document.getElementById("fullNameError").innerText =
+      "Only alphabets and spaces are allowed";
+    isValid = false;
+  }
+
+  if (!/^[A-Za-z\d\.\_]+@(gmail.com|outlook.com|ricr.in)$/.test(data.Email)) {
+    document.getElementById("emailError").innerText =
+      "Only standard Email format allowed with gmail.com";
+    isValid = false;
+  }
+
+  if (!/^[6-9]\d{9}$/.test(data.Phone)) {
+    document.getElementById("phoneError").innerText =
+      "Only Indian mobile numbers allowed";
+    isValid = false;
+  }
+
+  return isValid;
+}
--- jsAssignments/jsProjects/InstituteRegistrationForm/form.js
+++ jsAssignments/jsProjects/InstituteRegistrationForm/form.js
@@ -6,29 +6,139 @@ document
     const email = document.getElementById("email").value.trim();
     const MobileNumber = document.getElementById("number").value.trim();
     const dob = document.getElementById("dob").value.trim();
+    const city = document.getElementById("city").value.trim();
+    const residentialAddress = document
+      .getElementById("residentialAddress")
+      .value.trim();
+    const pinCode = document.getElementById("pinCode").value.trim();
     const gender = document.querySelector(
       "input[name='gender']:checked",
     )?.value;
-    const lastCompletedQualification = document.getElementById("lastCompletedQualification").value;
+    const lastCompletedQualification = document.getElementById(
+      "lastCompletedQualification",
+    ).value;
     const percentage = document.getElementById("percentage").value.trim();
     const preferredCourse = document.getElementById("preferredCourse").value;
     let selectedTimings = [];
-    const batchTimingPreference = document.querySelectorAll("input[name= 'batchTiming']:checked").forEach((item)=>{
+    const batchTimingPreference = document
+      .querySelectorAll("input[name= 'batchTiming']:checked")
+      .forEach((item) => {
         selectedTimings.push(item.value);
-    })
+      });
 
     let registrationFormData = {
-        fullName : fullName,
-        email : email,
-        MobileNumber : MobileNumber,
-        dob : dob,
-        gender : gender,
-        lastCompletedQualification : lastCompletedQualification,
-        percentage : percentage,
-        preferredCourse : preferredCourse,
-        selectedTimings : selectedTimings,
-    }
-    console.log("registrationFormData : " , registrationFormData);
-    
+      fullName: fullName,
+      email: email,
+      MobileNumber: MobileNumber,
+      dob: dob,
+      gender: gender,
+      lastCompletedQualification: lastCompletedQualification,
+      percentage: percentage,
+      preferredCourse: preferredCourse,
+      selectedTimings: selectedTimings,
+      residentialAddress: residentialAddress,
+      city: city,
+      pinCode: pinCode,
+    };
+    console.log("registrationFormData : ", registrationFormData);
+    validateData(registrationFormData); //validateData function calling
+  });
 
+function validateData(data) {
+  let isValid = true;
+  let nameReg = /^[A-Za-z\s]+$/;
+  let emailReg = /^[A-Za-z][A-Za-z\.\d]+@(gmail|outlook|ricr)(\.com|\.in)$/;
+  let numReg = /^[6-9]\d{9}$/;
+  let percentageReg = /^(A-F|\d[0-100])/;
+  let residentialAddressReg = /^[A-Za-z\d\.]+$/;
+  let cityReg = /^[A-Za-z\d\.\s]+$/;
+  let pincodeReg = /^[\d]{6}$/;
+
+  document.querySelectorAll(".manualError").forEach((items) => {
+    items.innerText = "";
+  });
+
+  if (!data.fullName) {
+    document.getElementById("fullNameError").innerHTML = "Full name required";
+    isValid = false;
+  } else if (!nameReg.test(data.fullName)) {
+    document.getElementById("fullNameError").innerText =
+      "Only letters and spaces allowed";
+    isValid = false;
+  }
+
+  if (!data.email) {
+    document.getElementById("emailError").innerHTML = "full email required";
+    isValid = false;
+  } else if (!emailReg.test(data.email)) {
+    document.getElementById("emailError").innerText = "Invalid email format";
+    isValid = false;
+  }
+
+  if (!data.MobileNumber) {
+    document.getElementById("numberError").innerHTML = "number required";
+    isValid = false;
+  } else if (!numReg.test(data.MobileNumber)) {
+    document.getElementById("numberError").innerText = "Invalid number format";
+    isValid = false;
+  }
+  if (!data.gender) {
+    document.getElementById("genderError").innerHTML = "Select Gender";
+    isValid = false;
+  }
+
+  // QUALIFICATION
+  if (!data.lastCompletedQualification) {
+    document.getElementById("qualificationError").innerHTML =
+      "Select Qualification";
+    isValid = false;
+  }
+
+  // COURSE
+  if (!data.preferredCourse) {
+    document.getElementById("courseError").innerHTML = "Select Course";
+    isValid = false;
+  }
+
+  // TIMINGS
+  if (data.selectedTimings.length === 0) {
+    document.getElementById("batchTimingError").innerHTML =
+      "Select at least one timing";
+    isValid = false;
+  }
+
+  // ADDRESS
+  if (!data.residentialAddress) {
+    document.getElementById("addressError").innerHTML = "Address Required";
+    isValid = false;
+  }
+
+  // CITY
+  if (!data.city) {
+    document.getElementById("cityError").innerHTML = "City Required";
+    isValid = false;
+  } else if (!cityReg.test(data.city)) {
+    document.getElementById("cityError").innerHTML = "Only letters allowed";
+    isValid = false;
+  }
+
+  // PIN CODE
+  if (!data.pinCode) {
+    document.getElementById("pinCodeError").innerHTML = "Pin Code Required";
+    isValid = false;
+  } else if (!pinReg.test(data.pinCode)) {
+    document.getElementById("pinCodeError").innerHTML =
+      "Enter valid 6-digit pin code";
+    isValid = false;
+  }
+
+  return isValid;
+}
+
+document
+  .getElementById("registrationForm")
+  .addEventListener("reset", function () {
+    document.querySelectorAll(".manualError").forEach((items) => {
+      items.innerText = "";
+    });
   });
