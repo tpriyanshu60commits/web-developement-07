42cc1716f77fa263d1e9c9e1d7cc50d0c48afdf4
tpriyanshu60commits <ppriyanshu.ttiwari@gmail.com>
2026-05-24 13:13:38 +0530

    weather app

---
+++ JSBasics/AdvanceJS/weatherApp/script.js
@@ -0,0 +1,34 @@
+const API_KEY = "185cdcd8598465c9626aa5b3388eecb6";
+
+async function getWeather() {
+  const cityName = document.getElementById("cityName").value.trim();
+
+  const { Lattitude, Longitude } = await getGeoLocation(cityName);
+
+  //   console.log({ Lattitude, Longitude });
+
+  const WEATHER_API = `https://api.openweathermap.org/data/2.5/weather?lat=${Lattitude}&lon=${Longitude}&appid=${API_KEY}`;
+
+  const response = await fetch(WEATHER_API);
+  const data = await response.json();
+
+  //console.log(data);
+
+  const temperature = data.main.temp - 273.15;
+
+  document.getElementById("Temperature").innerText = temperature.toFixed(2);
+}
+
+async function getGeoLocation(city) {
+  const GEO_LOC_API = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${API_KEY}`;
+
+  const response = await fetch(GEO_LOC_API);
+  const data = await response.json();
+
+  //   console.log(data);
+
+  const Lattitude = data[0].lat;
+  const Longitude = data[0].lon;
+
+  return { Lattitude, Longitude };
+}
