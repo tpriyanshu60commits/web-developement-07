8c77c25414147447e1280ecdeaeedd432a99e0e9
tpriyanshu60commits <ppriyanshu.ttiwari@gmail.com>
2026-05-31 00:44:04 +0530

    js class 21 revesion done

---
+++ jsPractise/projectsAgain/class21/operartors.js
@@ -0,0 +1,109 @@
+// Operators in JavaScript
+
+// 1.Create two variables and perform addition,
+// subtraction, multiplication, and division.
+
+let aq1 = 45;
+let bq1 = 45;
+console.log(aq1+bq1);
+console.log(aq1-bq1);
+console.log(aq1*bq1);
+
+// 2. Find the remainder when two numbers are
+// divided using the modulus operator.
+
+let aq2= 15;
+let bq2 = 7;
+console.log(aq1%bq2);
+
+
+
+
+
+
+// 3.Use the exponent operator to calculate power
+// of a number.
+let aq3 = 5**3;
+console.log(aq3);
+
+
+
+
+
+
+
+// 4.Assign a value to a variable and then update it
+// using += and -= operators.
+let aq4 =5;
+console.log(aq4+=6);
+
+
+
+
+
+
+
+// 5.Use *= and /= operators to modify a variable’s
+// value.
+let aq5 =5;
+console.log(aq4*=6);
+
+
+
+
+// 6.Compare two values using == and === and
+// observe the result.
+let aq6 = 45;
+let bq6 = "45";
+console.log(aq6==bq6);
+console.log(aq6===bq6);
+
+// 7.Use comparison operators (>, <, >=, <=) to check
+// different conditions.
+
+
+
+
+
+
+// 8.Create two variables and check if both
+// conditions are true using &&.
+
+
+
+
+
+// 9.Use ! operator to reverse a boolean value.
+
+
+
+
+
+// 10. Demonstrate pre-increment and postincrement and observe the difference.
+
+
+
+
+
+// 11. Demonstrate pre-decrement and postdecrement and observe the difference.
+
+
+
+
+
+// 12. Use the ternary operator to check if a number
+// is pass or fail (marks ≥ 40).
+
+
+
+
+
+// 13. Combine multiple conditions using logical
+// operators (like marks > 80 AND attendance > 75).
+
+
+
+
+
+// 14. Use typeof operator to check the data type of
+// different values
---
+++ jsPractise/projectsAgain/class21/script.js
@@ -0,0 +1,111 @@
+// Variables and Data Types in JavaScript
+
+// 1.Create variables to store your name, age, and
+// student status (true/false)
+let studentq1 = "priyanshu tiwari";
+let ageq1 = 24;
+if(ageq1==24)
+{
+    console.log(true);
+    
+}
+
+
+// 2.Declare variables using var, let, and const.
+
+var nameq2 = "amanq2";
+let nameq2ii = "amanq2";
+const nameq2iii = "amanq2";
+
+
+
+// 3.Declare a variable first and assign a value later
+// (declaration + initialization).
+var a;
+a=20;
+console.log(a);
+
+
+
+
+// 4.Create variables of different data
+// types: number, string, boolean.
+
+let q4i = "priyanshu";
+let q4ii = 24;
+let q4iii = true;
+console.log(q4iii);
+
+
+
+// 5.Create one variable with undefined and
+// another with null.
+q5a=200;
+var q5a;
+console.log(q5a);
+
+
+// 6.Use typeof to check the data type of different
+// variables.
+
+let q6 = typeof(q5a);
+console.log(q6);
+
+
+
+
+
+
+// 7.Create multiple variables and display their
+// values together.
+
+let q7i= 45;
+let q7ii= 75;
+let q7iii= 55;
+console.log(q7i ,q7ii, q7iii);
+
+
+
+
+
+
+// 8.Create variables and perform a simple
+// calculation (like addition).
+
+console.log(q7i+q7iii);
+
+
+
+
+// 9.Create an object to store student details (name
+// and age).
+
+const q9 = [
+    {
+        name: "priyanshu",
+        age : 25,
+    }
+];
+console.log(q9);
+
+
+
+
+
+
+// 10.Show dynamic typing by changing a variable
+// from number to string.
+
+
+var eq10 = 12;
+console.log(eq10);
+
+eq10 = "hello";
+console.log(eq10);
+
+
+
+
+
+// 11.Create a const variable and observe what
+// happens when you try to change its value.
