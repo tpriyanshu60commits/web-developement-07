44ee04590d2eae81dc3d21c84bcc49e0beeecda9
tpriyanshu60commits <ppriyanshu.ttiwari@gmail.com>
2026-06-22 16:42:47 +0530

    JSX (JavaScript XML) learning

--- Frontend-react/thapaPractise/thapaPractise/src/App.jsx
+++ Frontend-react/thapaPractise/thapaPractise/src/App.jsx
@@ -1,8 +1,19 @@
+import { useState } from "react";
+
 const App = ()=>
 {
+  const[ name , newname] = useState(" priyanshu tiwari"); 
+
   return(
     <>
-    <h1>hello , priyanshu tiwari</h1>
+    <h1>hello , {name}</h1>
+    <button onClick={()=>{newname("this is new one")}}>submit</button>
+    <hr />
+    <div>
+      <img src="favicon.svg" alt="" width={1000} height={400} />
+      <h2>react icon</h2>
+      <p>it is a react icon</p>
+    </div>
     </>
   )
 }
