4c7b2e154e42810d305f8658569df12cd6f3deab
tpriyanshu60commits <ppriyanshu.ttiwari@gmail.com>
2026-06-26 20:30:51 +0530

    code update backend

--- Backend/firstBackend/src/controllers/auth.controller.js
+++ Backend/firstBackend/src/controllers/auth.controller.js
@@ -38,8 +38,38 @@ export const RegisterUser = async (req, res) => {
   }
 };
 
-export const LoginUser = (req, res) => {
-  res.json({ message: "Login Successfull from Controller" });
+export const LoginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res.status(400).json({ message: "All fields required" });
+    }
+
+    const existingUser = await User.findOne({ email });
+
+    if (!existingUser) {
+      return res.status(404).json({ message: "Email not registered" });
+    }
+
+    const isMatch = await bcrypt.compare(password, existingUser.password);
+
+    if (!isMatch) {
+      return res.status(401).json({ message: "Incorrect password" });
+    }
+
+    res.status(200).json({
+      message: "Login successful",
+      user: {
+        id: existingUser._id,
+        fullName: existingUser.fullName,
+        email: existingUser.email,
+      },
+    });
+
+  } catch (error) {
+    res.status(500).json({ message: "Server error" });
+  }
 };
 
 export const LogoutUser = (req, res) => {
