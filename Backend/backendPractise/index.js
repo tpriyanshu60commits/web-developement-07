13ebd470be72e9a38d02fd4de633f2392ae3587b
tpriyanshu60commits <ppriyanshu.ttiwari@gmail.com>
2026-07-07 18:43:30 +0530

    understanding of morgan , cors , cookie parser

--- Backend/backendPractise/index.js
+++ Backend/backendPractise/index.js
@@ -1,43 +1,54 @@
-import dotenv from "dotenv";
-dotenv.config();
-console.log(process.env.port);
-console.log(process.pid);
+  import dotenv from "dotenv";
+  dotenv.config();
+  console.log(process.env.port);
+  console.log(process.pid);
 
-import express from "express";
-import authrouter from "./src/routers/auth.route.js";
-import testrouter from "./src/routers/test.route.js";
-import connectDB from "./src/config/dbConnection.config.js"
-const app = express();
-app.use(express.json());
+  import express from "express";
+  import authrouter from "./src/routers/auth.route.js";
+  import testrouter from "./src/routers/test.route.js";
+  import connectDB from "./src/config/dbConnection.config.js";
+  import cloudinary from "./src/config/cloudinary.config.js";
+  import morgan from "morgan";
+  import cors from "cors";
+  import cookieParser from "cookie-parser";
+  const app = express();
+  app.use(cors({
+    origin:"http://localhost:5173",
+    credentials:true,
+  }))
+  app.use(morgan("dev"))
+  app.use(express.json());
+  app.use(cookieParser())
+  app.use("/auth", authrouter);
+  app.use("/test", testrouter);
 
-app.use("/auth", authrouter);
-app.use("/test", testrouter);
+  app.post("/", (req, res) => {
+    console.log("this is start of code");
+    res.json({
+      message: "Welcome to my first backend Project",
+    });
+  });
 
-app.post("/",(req,res)=>
-{
-  console.log("this is start of code");
-  res.json({
-    message:"Welcome to my first backend Project"
-  })
-})
-
-// Error Handling Middleware
-app.use((err, req, res, next) => {
-
+  // Error Handling Middleware
+  app.use((err, req, res, next) => {
     const ErrMessage = err.message || "Internal Server Error";
     const ErrStatusCode = err.statusCode || 500;
 
     res.status(ErrStatusCode).json({
-        message: ErrMessage
+      message: ErrMessage,
     });
+  });
+  const PORT = process.env.port || 3000;
 
-});
-const PORT = process.env.port ||3000;
-
-
-
-app.listen(PORT, () => {
-  console.log("server started");
-  connectDB();
-});
-
+  app.listen(PORT, async () => {
+    console.log("server started");
+    await connectDB();
+    try {
+      const result = await cloudinary.api.ping();
+      console.log("cloudunary connection succeed");
+      console.log(result);
+    } catch (error) {
+      console.log(error.message);
+      process.exit(1);
+    }
+  });
