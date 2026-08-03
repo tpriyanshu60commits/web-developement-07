928f52b76cfd904ec8f7ec3c4cfcd6b9680cefe5
tpriyanshu60commits <ppriyanshu.ttiwari@gmail.com>
2026-08-03 16:19:09 +0530

    register practise done

---
+++ Backend/backendPractise/client/src/context/AuthContext.jsx
@@ -0,0 +1,18 @@
+import { React, createContext, useState, useEffect, useContext } from "react";
+
+const AuthContext = createContext();
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(
+    JSON.parse(sessionStorage.getItem("cravingUser")) || null,
+  );
+  const [isLogin, setIsLogin] = useState(!!user);
+  const [role, setRole] = useState(user ? user.userType : null);
+  useEffect(() => {
+    (setIsLogin(!!user), setRole(user ? user.userType : null));
+  }, [user]);
+  const value = { isLogin, setIsLogin, user, setUser, role, setRole };
+  return (
+    <AuthContext.Provider value={value}>{children}</AuthContext.Provider>
+  );
+};
+export const useAuth = () => useContext(AuthContext);
--- Backend/backendPractise/client/src/pages/Register.jsx
+++ Backend/backendPractise/client/src/pages/Register.jsx
@@ -1,102 +1,104 @@
-import React from "react";
-import api from "../config/ApiConfig";
-import { useState } from "react";
+import React, { useState } from "react";
 import toast from "react-hot-toast";
+import { Link, useParams, useNavigate } from "react-router-dom";
+import api from "../config/ApiConfig";
+
 const Register = () => {
   const [formData, setFormData] = useState({
-    userType: "",
-    email: "",
+    userType: "customer",
     fullName: "",
+    email: "",
     phone: "",
     gender: "",
     dob: "",
     password: "",
     confirmPassword: "",
-    agreeTerms: "",
+    agreeTerms: false,
   });
-  const [errors, setErrors] = useState({});
-  const [isloading, setIsLoading] = useState(false);
 
-  const handleInputChange = (e) => {
-    const {name, value, type, checked} = e.target;
+  const [errors, setErrors] = useState({});
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleInputData = (e) => {
+    console.log(e.target.name, e.target.value);
+    const { name, value, checked, type } = e.target;
     setFormData((prev) => ({
       ...prev,
       [name]: type === "checkbox" ? checked : value,
     }));
-  };
-
-  const handleUserTypeChange = (e) => {
-    setFormData((prev) => ({
+    setErrors((prev) => ({
       ...prev,
-      userType: e.target.value,
+      [name]: "",
     }));
   };
 
   const validateForm = (data) => {
     const newErrors = {};
-    if (!data.fullName.trim()) newErrors.fullName = "FullName is required";
-    if (!data.email.trim()) newErrors.email = "email is required";
-    if (!data.phone.trim()) newErrors.phone = "phone is required";
-    if (!data.gender) newErrors.gender = "gender is required";
-    if (!data.password || data.password.length < 6)
-      newErrors.password = "password must be of atleast 6 characters";
+    if (!data.fullName.trim()) newErrors.fullName = "FullName is Required";
+    if (!data.phone.trim()) newErrors.phone = "Phone is Required";
+    if (!data.email.trim()) newErrors.email = "Email is Required";
+    if (!data.gender) newErrors.gender = "Gender is Required";
+    if (!data.dob) newErrors.dob = "DOB is Required";
     if (!data.confirmPassword)
-      newErrors.confirmPassword = "confirmPassword is required";
+      newErrors.confirmPassword = "confirmPassword is Required";
+    if (!data.agreeTerms) newErrors.agreeTerms = "agreeTerms is Required";
+    if (!data.password || data.password.length < 6)
+      newErrors.password = "Password must be at least 6 characters";
     if (data.password !== data.confirmPassword)
-      newErrors.confirmPassword = "Password do not match";
-    if (!data.agreeTerms)
-      newErrors.agreeTerms = "You must agree to the terms and condition";
-
+      newErrors.confirmPassword = "Passwords do not match";
     return newErrors;
   };
-
   const handleSubmit = async (e) => {
     e.preventDefault();
-    setErrors({});
     setIsLoading(true);
+    setErrors(false);
 
     const validateErrors = validateForm(formData);
     if (Object.keys(validateErrors).length > 0) {
       setErrors(validateErrors);
       setIsLoading(false);
+
       return;
     }
-    console.log("FormData", formData);
+    console.log("Form submitted:", formData);
+    const payload = {
+      ...formData,
+      email: formData.email.toLowerCase(),
+    };
+    console.log(payload);
+
     try {
-      const res = await api.post("/auth/register", {
-        ...formData,
-        email: formData.email.toLowerCase(),
-      });
-      console.log(res);
-      console.log(res.data);
-      console.log(res.data.message);
-      toast.success(res?.data?.message);
+      const res = await api.post("/auth/register", payload);
+      toast.success(res.data.message);
     } catch (error) {
       toast.error(
         error.response?.data?.message ||
           "Unknown error occurred during registration. Please try again.",
       );
-    }
-    finally{
+    } finally {
       setIsLoading(false);
     }
   };
 
   return (
     <>
-      <div className="bg-(--color-base-100) h-[90vh] flex items-center justify-center">
+      <div className="h-screen bg-blue-500 flex items-center justify-end bg-cover bg-center p-10 md:pe-30">
         <div className="bg-white rounded-lg shadow-md px-10 py-6 max-w-md w-full overflow-y-auto max-h-[85vh]">
-          <p className="text-(--color-secondary) text-center mb-4 ">
+          <h1 className="text-3xl font-bold text-(--color-primary) mb-2 text-center">
+            Create Account
+          </h1>
+          <p className="text-(--color-secondary) text-center mb-4">
             Join us as a Customer, Restaurant, or Rider
           </p>
-          {/* usertype */}
-          <div>
-            <label className="text-(--color-neutral) font-semibold mb-3">
+
+          <div className="mb-6">
+            <label className="block text-(--color-neutral) font-semibold mb-3">
               Register as:
             </label>
             <div className="flex gap-5">
               {["customer", "restaurant", "rider"].map((type) => (
                 <label
+                  htmlFor=""
                   key={type}
                   className="flex items-center gap-2 cursor-pointer"
                 >
@@ -104,160 +106,162 @@ const Register = () => {
                     type="radio"
                     name="userType"
                     value={type}
+                    className="cursor-pointer"
+                    onChange={handleInputData}
                     checked={formData.userType === type}
-                    onChange={handleUserTypeChange}
                   />
-                  <span className="text-(--color-neutral) capitalize">
-                    {type}
-                  </span>
+                  {type}
                 </label>
               ))}
             </div>
-            {/* fullName */}
-            <form onSubmit={handleSubmit}>
-              <div className="mt-3 mb-3">
-                <input
-                  type="text"
-                  name="fullName"
-                  placeholder="Enter your fullName"
-                  value={formData.fullName}
-                  onChange={handleInputChange}
-                  className="w-full px-3 py-2 border rounded-md text-sm text-(--color-neutral) placeholder-gray-500 focus:outline-none focus:ring-1 focus:ring-(--color-primary) "
-                />
-                {errors.fullName && (
-                  <span className="text-(--color-error) text-xs mt-1 block">
-                    {errors.fullName}
-                  </span>
-                )}
-              </div>
-              {/* email */}
-              <div className="mt-3 mb-3">
-                <input
-                  type="email"
-                  name="email"
-                  placeholder="Enter your email"
-                  value={formData.email}
-                  onChange={handleInputChange}
-                  className="w-full px-3 py-2 border rounded-md text-sm text-(--color-neutral) placeholder-gray-500 focus:outline-none focus:ring-1 focus:ring-(--color-primary) "
-                />
-                {errors.email && (
-                  <span className="text-(--color-error) text-xs mt-1 block">
-                    {errors.email}
-                  </span>
-                )}
-              </div>
-              {/* phone */}
-              <div className="mt-3 mb-3">
-                <input
-                  type="tel"
-                  name="phone"
-                  placeholder="Enter your phone"
-                  value={formData.phone}
-                  onChange={handleInputChange}
-                  className="w-full px-3 py-2 border rounded-md text-sm text-(--color-neutral) placeholder-gray-500 focus:outline-none focus:ring-1 focus:ring-(--color-primary) "
-                />
-                {errors.phone && (
-                  <span className="text-(--color-error) text-xs mt-1 block">
-                    {errors.phone}
-                  </span>
-                )}
-              </div>
-              {/* gender */}
-              <div className="mt-3 mb-3">
-                <select
-                  name="gender"
-                  value={formData.gender}
-                  onChange={handleInputChange}
-                  className="w-full px-3 py-2 border rounded-md text-sm text-(--color-neutral) placeholder-gray-500 focus:outline-none focus:ring-2 focus:ring-(--color-primary)"
-                >
-                  <option value="">Select gender</option>
-                  <option value="male">Male</option>
-                  <option value="female">Female</option>
-                  <option value="other">Other</option>
-                </select>
-                {errors.gender && (
-                  <span className="text-(--color-error) text-xs mt-1 block">
-                    {errors.gender}
-                  </span>
-                )}
-              </div>
-              {/* dob */}
-              <div className="mt-3 mb-3">
-                <input
-                  type="date"
-                  name="dob"
-                  placeholder="Enter your dob"
-                  value={formData.dob}
-                  onChange={handleInputChange}
-                  className="w-full px-3 py-2 border rounded-md text-sm text-(--color-neutral) placeholder-gray-500 focus:outline-none focus:ring-1 focus:ring-(--color-primary) "
-                />
-                {errors.dob && (
-                  <span className="text-(--color-error) text-xs mt-1 block">
-                    {errors.dob}
-                  </span>
-                )}
-              </div>
-              {/* password */}
-              <div className="mt-3 mb-3">
-                <input
-                  type="password"
-                  name="password"
-                  placeholder="Enter your password"
-                  value={formData.password}
-                  onChange={handleInputChange}
-                  className="w-full px-3 py-2 border rounded-md text-sm text-(--color-neutral) placeholder-gray-500 focus:outline-none focus:ring-1 focus:ring-(--color-primary) "
-                />
-                {errors.password && (
-                  <span className="text-(--color-error) text-xs mt-1 block">
-                    {errors.password}
-                  </span>
-                )}
-              </div>
-              {/*confirm password */}
-              <div className="mt-3 mb-3">
-                <input
-                  type="password"
-                  name="confirmPassword"
-                  placeholder="Enter your confirm Password"
-                  value={formData.confirmPassword}
-                  onChange={handleInputChange}
-                  className="w-full px-3 py-2 border rounded-md text-sm text-(--color-neutral) placeholder-gray-500 focus:outline-none focus:ring-1 focus:ring-(--color-primary) "
-                />
-                {errors.confirmPassword && (
-                  <span className="text-(--color-error) text-xs mt-1 block">
-                    {errors.confirmPassword}
-                  </span>
-                )}
-              </div>
-              {/*agree terms */}
-              <div className="mt-3 mb-3">
-                <input
-                  type="checkbox"
-                  name="agreeTerms"
-                  value={formData.agreeTerms}
-                  onChange={handleInputChange}
-                  className="mt-1 cursor-pointer"
-                />
-                <span className="text-sm ml-2">
-                  I agree to the{" "}
-                  <span className="text-(--color-primary) hover:underline">
-                    terms and conditions.
-                  </span>
-                </span>
-                {errors.agreeTerms && (
-                  <span className="text-(--color-error) text-xs mt-1 block">
-                    {errors.agreeTerms}
-                  </span>
-                )}
-              </div>
-              <button
-                type="submit"
-                className="w-full py-3 bg-(--color-primary) text-white font-semibold rounded-md hover:bg-orange-700 transition-colors duration-300 mb-4"
-              >
-                {isloading ? "Registering..." : "Register"}
-              </button>
-            </form>
           </div>
+
+          {/* Full Name */}
+          <form onSubmit={handleSubmit}>
+            <div className="mb-6">
+              <input
+                type="text"
+                name="fullName"
+                value={formData.fullName}
+                className="w-full px-3 py-2 border rounded-md text-sm text-(--color-neutral) placeholder-gray-500 focus:outline-none focus:ring-2 focus:ring-(--color-primary)"
+                onChange={handleInputData}
+                placeholder="Enter your fullName"
+              />
+              {errors.fullName && (
+                <span className="text-(--color-error) text-xs mt-1 block">
+                  {errors.fullName}
+                </span>
+              )}
+            </div>
+            {/* Email */}
+            <div className="mb-4">
+              <input
+                type="email"
+                name="email"
+                value={formData.email}
+                onChange={handleInputData}
+                placeholder="Enter your email"
+                className="w-full px-3 py-2 border rounded-md text-sm text-(--color-neutral) placeholder-gray-500 focus:outline-none focus:ring-2 focus:ring-(--color-primary)"
+              />
+              {errors.email && (
+                <span className="text-(--color-error) text-xs mt-1 block">
+                  {errors.email}
+                </span>
+              )}
+            </div>
+
+            {/* Phone */}
+            <div className="mb-4">
+              <input
+                type="number"
+                name="phone"
+                value={formData.phone}
+                onChange={handleInputData}
+                placeholder="Enter your phone number"
+                className="w-full px-3 py-2 border rounded-md text-sm text-(--color-neutral) placeholder-gray-500 focus:outline-none focus:ring-2 focus:ring-(--color-primary)"
+              />
+              {errors.phone && (
+                <span className="text-(--color-error) text-xs mt-1 block">
+                  {errors.phone}
+                </span>
+              )}
+            </div>
+            {/* gender */}
+            <div className="mb-4">
+              <select
+                name="gender"
+                value={formData.gender}
+                onChange={handleInputData}
+                className="w-full px-3 py-2 border rounded-md text-sm text-(--color-neutral) placeholder-gray-500 focus:outline-none focus:ring-2 focus:ring-(--color-primary)"
+              >
+                <option value="">Select gender</option>
+                <option value="male">Male</option>
+                <option value="female">Female</option>
+                <option value="other">Other</option>
+              </select>
+
+              {errors.gender && (
+                <span className="text-(--color-error) text-xs mt-1 block">
+                  {errors.gender}
+                </span>
+              )}
+            </div>
+            {/* Dob */}
+            <div className="mb-4">
+              <input
+                type="date"
+                name="dob"
+                value={formData.dob}
+                onChange={handleInputData}
+                placeholder="Enter your dob"
+                className="w-full px-3 py-2 border rounded-md text-sm text-(--color-neutral) placeholder-gray-500 focus:outline-none focus:ring-2 focus:ring-(--color-primary)"
+              />
+              {errors.dob && (
+                <span className="text-(--color-error) text-xs mt-1 block">
+                  {errors.dob}
+                </span>
+              )}
+            </div>
+            {/* Password */}
+            <div className="mb-4">
+              <input
+                type="password"
+                name="password"
+                value={formData.password}
+                onChange={handleInputData}
+                placeholder="Enter your password"
+                className="w-full px-3 py-2 border rounded-md text-sm text-(--color-neutral) placeholder-gray-500 focus:outline-none focus:ring-2 focus:ring-(--color-primary)"
+              />
+              {errors.password && (
+                <span className="text-(--color-error) text-xs mt-1 block">
+                  {errors.password}
+                </span>
+              )}
+            </div>
+            {/* Confirm Password */}
+            <div className="mb-6">
+              <input
+                type="text"
+                name="confirmPassword"
+                value={formData.confirmPassword}
+                onChange={handleInputData}
+                placeholder="Confirm your password"
+                className="w-full px-3 py-2 border rounded-md text-sm text-(--color-neutral) placeholder-gray-500 focus:outline-none focus:ring-2 focus:ring-(--color-primary)"
+              />
+              {errors.confirmPassword && (
+                <span className="text-(--color-error) text-xs mt-1 block">
+                  {errors.confirmPassword}
+                </span>
+              )}
+            </div>
+            {/* agreeTerms */}
+            <div className="mb-6">
+              <input
+                type="checkbox"
+                name="agreeTerms"
+                checked={formData.agreeTerms}
+                className="mt-1 cursor-pointer"
+                onChange={handleInputData}
+              />
+              <span className="text-sm">
+                I agree to the{" "}
+                <span className="text-(--color-primary) hover:underline">
+                  terms and conditions.
+                </span>
+              </span>
+              {errors.agreeTerms && (
+                <span className="text-(--color-error) text-xs mt-1 block ml-7">
+                  {errors.agreeTerms}
+                </span>
+              )}
+            </div>
+            <button
+              type="submit"
+              className="w-full py-3 bg-(--color-primary) text-white font-semibold rounded-md hover:bg-orange-700 transition-colors duration-300 mb-4 cursor-pointer"
+            >
+              {isLoading ? "Saving..." : "Save"}
+            </button>
+          </form>
         </div>
       </div>
     </>
